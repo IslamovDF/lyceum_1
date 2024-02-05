4724939f60da5d55ab363814eb39797af12cfb37
IslamovDF <IslamovDF@yandexlyceum.ru>
2024-02-05 12:08:38 +0500

    add second form for editing

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import sys
 import sqlite3
 from PyQt5 import uic
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QWidget
+
+
+class edit_form(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('edit.ui', self)
+        # self.saveBtn.clicked.connect(self.save_data)
+
 
 
 class Coffee(QMainWindow):
@@ -10,19 +18,60 @@ class Coffee(QMainWindow):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.load_data()
+        self.edit_f = edit_form()
+        self.edit_f.saveBtn.clicked.connect(self.save_data)
+        self.tableWidget.itemClicked.connect(self.edit)
+
+    def edit(self, values):
+        # получаем ID из стоки по которой кликнули
+        self.id_item = self.tableWidget.item(values.row(), 0).text()
+        self.edit_f.setWindowTitle(f'Редактирование записи c id = {values.row()}')
+
+        db = sqlite3.connect('coffee.sqlite')
+        db.cursor()
+        self.request2 = db.execute(f"""SELECT * FROM coffee WHERE id = {self.id_item}""").fetchall()
+        print(self.request2)
+        self.edit_f.tableEdit.setColumnCount(7)  # количество столбцов
+        self.edit_f.tableEdit.setRowCount(1)  # количество строк
+        self.edit_f.tableEdit.setHorizontalHeaderLabels(['ID', 'variety', 'roasting', 'type',
+                                                    'description', 'price', 'volume'])
+
+        for j, col in enumerate(self.request2[0]):
+            print(j, col)
+            self.edit_f.tableEdit.setItem(0, j, QTableWidgetItem(str(col)))
+        self.edit_f.show()
+        db.close()
 
     def load_data(self):
         db = sqlite3.connect('coffee.sqlite')
         db.cursor()
-        request = db.execute(f"""SELECT * FROM coffee""").fetchall()
+        self.request = db.execute(f"""SELECT * FROM coffee""").fetchall()
         self.tableWidget.setColumnCount(7)
-        self.tableWidget.setRowCount(len(request))
+        self.tableWidget.setRowCount(len(self.request))
         self.tableWidget.setHorizontalHeaderLabels(['ID', 'variety', 'roasting', 'type',
                                                     'description', 'price', 'volume'])
-        for i, row in enumerate(request):
+        for i, row in enumerate(self.request):
             for j, col in enumerate(row):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(col)))
+        db.close()
 
+    def save_data(self):
+        db2 = sqlite3.connect('coffee.sqlite')
+        db2.cursor()
+        self.request2 = db2.execute(f"""UPDATE coffee SET 
+                                    id = '{self.edit_f.tableEdit.item(0, 0).text()}',
+                                    variety = '{self.edit_f.tableEdit.item(0, 1).text()}',
+                                    roasting = '{self.edit_f.tableEdit.item(0, 2).text()}',
+                                    type = '{self.edit_f.tableEdit.item(0, 3).text()}',
+                                    description = '{self.edit_f.tableEdit.item(0, 4).text()}',
+                                    price = '{self.edit_f.tableEdit.item(0, 5).text()}',
+                                    volume = '{self.edit_f.tableEdit.item(0, 6).text()}'
+                                    WHERE id = '{self.edit_f.tableEdit.item(0, 0).text()}'""")
+        db2.commit()
+
+        db2.close()
+        self.load_data()
+        self.edit_f.close()
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
